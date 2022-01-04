c997e046207031c02bd5493fe62c6488b0768ccc
Namr2789 <83170416+Namr2789@users.noreply.github.com>
2022-01-04 10:16:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+webcam.attach( '#camera' );
+
+function takeSnapshot(){
+ webcam.snap(function(data_uri){
+     document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+});
+}
+
+console.log('ml5version:',ml5version);
+
+classifier = ml5.imageclassifier('https://teachablemachine.withgoogle.com/models/[...]',modelLoaded)
+
+function modelLoaded(){
+    console.log("model loaded !")
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult)
+}
+
+function gotResult(error, results){
+if (error) {
+    console.error(error);
+}
+else {
+    console.log(results)
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
+}
